ae7deb58318913940310f6828495463b2ad5db5e
nilteras <niltonrnascimento@hotmail.com>
2023-06-24 11:13:19 -0300

    POST /sign-up

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
- import express from "express"
+import express from "express"
 
- const app = express()
- app.use(express.json())
+const app = express()
+app.use(express.json())
+
+const user = [
+    {
+        username: 'bobesponja',
+        avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png"
+    }
+];
+
+const tweet = [
+    {
+        username: "bobesponja",
+        tweet: "Eu amo hambúrguer de siri!"
+    }
 
 
- 
+];
 
- const PORT = 5000
- app.listen(PORT, () => console.log(`Servidor iniciado na porta ${PORT}`))
+app.post('/sign-up', (req,res) => {
+    const { username, avatar } = req.body
+    const dataUser = (username,avatar)
+    user.push(dataUser)
+    res.send("OK")
+})
+
+app.post('/tweets', (req,res) => {
+    
+})
+
+app.get('/tweets', (req,res) => {
+    
+})
+
+
+const PORT = 5000
+app.listen(PORT, () => console.log(`Servidor iniciado na porta ${PORT}`))
